4bd5ca8099319e4398fa9abc5d52388e5b6a59a4
chapering <chapering@gmail.com>
2016-02-11 23:31:08 -0500

    updated scripts and results

--- scripts/edgefreqRanking-scatter.R
+++ scripts/edgefreqRanking-scatter.R
@@ -30,7 +30,22 @@ pdf("./edgeFreq-scatter.pdf",width=4.0,height=3.0)
 i=1
 colors<-c("red","blue","black","green","yellow","darkorange","darkorchid","gold4","darkgrey")
 pches<-c(0:8)
-for (key in ls(catdata)) {
+
+#3rdLib -> 3rdLib 
+#3rdLib -> SDK 
+#3rdLib -> UserCode 
+#SDK -> 3rdLib 
+#SDK -> SDK 
+#SDK -> UserCode 
+#UserCode -> 3rdLib 
+#UserCode -> SDK 
+#UserCode -> UserCode
+
+allkeys<-c('SDK -> SDK', '3rdLib -> SDK', '3rdLib -> 3rdLib', 'UserCode -> SDK', 
+		   'UserCode -> UserCode', 'UserCode -> 3rdLib', 'SDK -> 3rdLib', '3rdLib -> UserCode', 'SDK -> UserCode')
+cat( ls(catdata) )
+#for (key in ls(catdata)) {
+for (key in rev(allkeys)) {
 	#print (key)
 	vdata <- catdata[[key]]
 	#print (vdata)
@@ -39,16 +54,19 @@ for (key in ls(catdata)) {
 		#plot (x=c(1:length(vdata)), y=order(vdata), col=colors[i], log="xy", xlim=c(1,length(vdata)),ylim=c(1,max(vdata)))
 		#plot (c(1:length(vdata)), sort(vdata), col=colors[i], log="xy",pch=pches[i],xlim=c(1,1000000), ylim=c(1,1000000), xlab="Call",ylab="Frequency", cex=.1, cex.axis=.4, lwd=0.3, cex.lab=0.5)
 		#plot (c(1:length(vdata)), sort(vdata), col=colors[i], log="xy",pch=pches[i],xlim=c(1,700000), ylim=c(1,900000), ann=FALSE, cex=.1, cex.axis=.4, lwd=0.3, cex.lab=0.5)
-		plot (c(1:length(vdata)), sort(vdata), col=colors[i], log="xy",pch=pches[i], xlim=c(1,700000), ylim=c(1,900000), ann=FALSE, cex=.1, cex.axis=.4, lwd=0.3, cex.lab=0.5)
-		legend("topleft", legend=ls(catdata), cex=.3, col=colors, lwd=0.5, bty="n",pch=pches)
-		mtext(side = 1, text = "Call", line = .5, cex=.4)
-		mtext(side = 2, text = "Frequency", line = 1.0, cex=.4)
+		#plot (c(1:length(vdata)), sort(vdata), col=colors[i], log="xy",pch=pches[i], xlim=c(1,700000), ylim=c(1,900000), ann=FALSE, cex=.1, cex.axis=.4, lwd=0.3, cex.lab=0.5)
+		plot (c(1:length(vdata)), sort(vdata), col=colors[i], log="xy",pch='.', xlim=c(1,700000), ylim=c(1,900000), ann=FALSE, cex=.1, cex.axis=.4, lwd=0.3, cex.lab=0.5)
+		#legend("topleft", legend=ls(catdata), cex=.3, col=colors, lwd=0.5, bty="n",pch=pches)
+		legend("topleft", legend=rev(allkeys), cex=.4, col=colors, lwd=1.0, bty="n")
+		mtext(side = 1, text = "Call", line = .5, cex=.5)
+		mtext(side = 2, text = "Frequency", line = 1.0, cex=.5)
 		#axis(1, line=0.2)
 		#axis(2, line=0.2)
 	}
 	else {
 		#points(x=c(1:length(vdata)), y=order(vdata), col=colors[i], xlim=c(1,length(vdata)),ylim=c(1,max(vdata)))
-		points(sort(vdata), col=colors[i],pch=pches[i], cex=.1)
+		#points(sort(vdata), col=colors[i],pch=pches[i], cex=.1)
+		points(sort(vdata), col=colors[i],pch='.', cex=.1)
 	}
 	i <- i+1
 }
