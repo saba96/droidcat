8c8c90fdfd7bdfbdfb33a541f9328016fd659438
chapering <chapering@gmail.com>
2016-06-29 23:32:33 -0400

    up to when the full benign set is processed

--- ML/multipleModelsWithTopFeaturesOnly_tab.py
+++ ML/multipleModelsWithTopFeaturesOnly_tab.py
@@ -19,8 +19,9 @@ import string
 from configs import *
 from featureLoader import *
 
+
 # 10-fold cross-validation
-def cv(model, features, labels):
+def cvScore(model, features, labels):
     k=10
 
     selector = RFECV(model, step=1, cv=k)
@@ -44,6 +45,68 @@ def cv(model, features, labels):
            (score_rec, selector_rec.n_features_, selector_rec.ranking_), \
            (score_f1, selector_f1.n_features_, selector_f1.ranking_)
 
+def cv(model, features, labels):
+    global g_binary
+    k=10
+
+    selector = RFECV(model, step=1, cv=k)
+    selector = selector.fit (features, labels)
+    score = selector.score (features, labels)
+
+    selector_prec = RFECV(model, step=1, cv=k, scoring='precision_weighted')
+    selector_rec = RFECV(model, step=1, cv=k, scoring='recall_weighted')
+    selector_f1 = RFECV(model, step=1, cv=k, scoring='f1_weighted')
+
+    r=len(features)
+    subsize = r/k
+    subsamples=list()
+    sublabels=list()
+    for j in range(0,k):
+        subsamples.append( (features[j*subsize:(j+1)*subsize]) )
+        sublabels.append( (labels[j*subsize:(j+1)*subsize]) )
+
+    precision = 0.0
+    recall = 0.0
+    f1s = 0.0
+    for j in range(0,k):
+        testFeatures = subsamples[j]
+        testLabels = sublabels[j]
+        trainFeatures = list()
+        trainLabels = list()
+        for r in range(0,k):
+            if r==j:
+                continue
+            #trainFeatures.append( subsamples[r] )
+            #trainLabels.append( sublabels[r] )
+            for fl in subsamples[r]:
+                trainFeatures.append(fl)
+            for lal in sublabels[r]:
+                trainLabels.append( lal )
+        selector_prec.fit( trainFeatures, trainLabels )
+        selector_rec.fit( trainFeatures, trainLabels )
+        selector_f1.fit( trainFeatures, trainLabels )
+
+        y_pred_prec = selector_prec.predict( testFeatures )
+        y_pred_rec = selector_rec.predict( testFeatures )
+        y_pred_f1 = selector_f1.predict( testFeatures )
+        if g_binary:
+            prec=precision_score(testLabels, y_pred_prec, average='binary', pos_label='MALICIOUS')
+            rec=recall_score(testLabels, y_pred_rec, average='binary', pos_label='MALICIOUS')
+            f1=f1_score(testLabels, y_pred_f1, average='binary', pos_label='MALICIOUS')
+        else:
+            prec=precision_score(testLabels, y_pred_prec, average='weighted')
+            rec=recall_score(testLabels, y_pred_rec, average='weighted')
+            f1=f1_score(testLabels, y_pred_f1, average='weighted')
+
+        precision += prec
+        recall += rec
+        f1s += f1
+
+    return (score, selector.n_features_, selector.ranking_), \
+           (precision/k, selector_prec.n_features_, selector_prec.ranking_), \
+           (recall/k, selector_rec.n_features_, selector_rec.ranking_), \
+           (f1s/k, selector_f1.n_features_, selector_f1.ranking_)
+
 def selectFeatures(features, selection):
     featureSelect=[idx-1 for idx in selection]
     selectedfeatures=list()
@@ -52,6 +115,7 @@ def selectFeatures(features, selection):
     return selectedfeatures
     
 if __name__=="__main__":
+    global g_binary
     g_binary = False # binary or multiple-class classification
     if len(sys.argv)>=2:
         g_binary = sys.argv[1].lower()=='true'
@@ -80,29 +144,15 @@ if __name__=="__main__":
         for fset in (FSET_FULL, FSET_G, FSET_ICC, FSET_SEC, FSET_Y, FSET_YY, FSET_YYY):
             ret = cv (model, selectFeatures( features, fset ), labels)
             model2ret[str(model)+str(fset)] = ret
-    s=1
-    for fset in (FSET_FULL, FSET_G, FSET_ICC, FSET_SEC, FSET_Y, FSET_YY, FSET_YYY):
-        print "=========== feature set %d ===========" % s
-        fh = None
-        if g_binary:
-            fh = file ('binary-fs'+str(s)+'.txt', 'w')
-        else:
-            fh = file ('multiclass-fs'+str(s)+'.txt', 'w')
-        s+=1
-        for r in range(0, len(fset)):
+    tlabs=('accuracy', 'precision', 'recall', 'F1')
+    for i in (0,1,2,3):
+        print "******     " + str(tlabs[i]) + "    ******"
+        for fset in (FSET_FULL, FSET_G, FSET_ICC, FSET_SEC, FSET_Y, FSET_YY, FSET_YYY):
             for model in models:
                 ret = model2ret[str(model)+str(fset)]
-                for i in (0,1,2,3):
-                    assert len(fset)==len(ret[i][2])
-                    print >> fh, "%s\t" % (ret[i][2][r]),
-            print >> fh
-        print >> fh
-        for model in models:
-            ret = model2ret[str(model)+str(fset)]
-            for i in (0,1,2,3):
-                print >> fh, "%s\t" % (ret[i][1]),
-        print >> fh
-        fh.close()
+                print "%s\t" % ret[i][0],
+            print
+        print
 
     sys.exit(0)
 
