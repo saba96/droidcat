27882293f6a096b17fcec5684a4950b23e33904e
hcai <hcai@eecs.wsu.edu>
2017-04-06 12:08:23 -0700

    remove redundant files

--- artifacts/droidcat/scripts/allICCReport_all.sh
+++
@@ -1,7 +0,0 @@
-allICCReport.sh first_rep implicit
-allICCReport.sh second_rep implicit
-allICCReport.sh third_rep implicit
-allICCReport.sh first_rep explicit 
-allICCReport.sh second_rep explicit 
-allICCReport.sh third_rep explicit
-
