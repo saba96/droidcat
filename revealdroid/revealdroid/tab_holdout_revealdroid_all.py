bbcc46056a595c35ca82e96bf6d63b031a9ef91d
Haipeng Cai <hcai@hcai-dl580>
2018-08-18 14:44:35 -0700

    added code for computing same-period results of revealdroid

--- revealdroid/revealdroid/tab_holdout_revealdroid_all.py
+++ revealdroid/revealdroid/tab_holdout_revealdroid_all.py
@@ -38,22 +38,86 @@ def varname(p):
         if m:
             return m.group(1)
 
-def span_detect(model, trainfeatures, trainlabels, testfeatures, testlabels):
+def holdout(model, features, labels):
+    sr=len(features)
+    assert sr==len(labels)
 
-    print >> sys.stdout, "%d samples for training, %d samples for testing" % (len (trainfeatures), len(testfeatures))
+    uniqLabels = set()
+    for item in labels:
+        uniqLabels.add (item)
 
+    lab2idx=dict()
+    for k in range(0, sr):
+        lab = labels[k]
+        if lab not in lab2idx.keys():
+            lab2idx[lab] = list()
+        lab2idx[lab].append (k)
+
+    testfeatures=list()
+    testlabels=list()
+
+    allidx2rm=list()
+    for lab in lab2idx.keys():
+        sz = len(lab2idx[lab])
+        nrm = int(sz*HOLDOUT_RATE);
+        idxrm = set()
+        while len(idxrm) < nrm:
+            t = random.randint(0,sz-1)
+            idxrm.add ( lab2idx[lab][t] )
+        for idx in idxrm:
+            testfeatures.append ( features[idx] )
+            testlabels.append ( labels[idx] )
+            allidx2rm.append(idx)
+
+    trainfeatures=list()
+    trainlabels=list()
+    for l in range(0, sr):
+        if l in allidx2rm:
+            continue
+        trainfeatures.append(features[l])
+        trainlabels.append(labels[l])
+
+    print >> sys.stdout, "%d samples for training, %d samples held out will be used for testing" % (len (trainfeatures), len(testfeatures))
+
+    predicted_labels=list()
     model.fit ( trainfeatures, trainlabels )
 
-    y_pred = model.predict ( testfeatures )
+    for j in range(0, len(testlabels)):
+        y_pred = model.predict( [testfeatures[j]] )
+        #print >> sys.stderr, "j=%d, testLabels: %s" % (j, str(testlabels[j]))
+        #print >> sys.stderr, "j=%d, predicted: %s" % (j, str(y_pred))
 
-    if g_binary:
+        predicted_labels.append ( y_pred )
+
+    '''
+    for i in range(0, len(predicted_labels)):
+        #print type(predicted_labels[i])
+        if predicted_labels[i][0] not in big_families:
+            predicted_labels[i] = numpy.array(['MALICIOUS'])
+    '''
+
+    #print "%s\n%s\n" % (str(sublabels), str(predicted_labels))
+    #big_families=["DroidKungfu", "ProxyTrojan/NotCompatible/NioServ", "GoldDream", "Plankton", "FakeInst", "BENIGN", "MALICIOUS"]
+
+    y_pred = predicted_labels
+
+    #if g_binary:
+    if False:
         prec=precision_score(testlabels, y_pred, average='binary', pos_label='MALICIOUS')
         rec=recall_score(testlabels, y_pred, average='binary', pos_label='MALICIOUS')
         f1=f1_score(testlabels, y_pred, average='binary', pos_label='MALICIOUS')
+
     else:
         prec=precision_score(testlabels, y_pred, average='weighted')
         rec=recall_score(testlabels, y_pred, average='weighted')
         f1=f1_score(testlabels, y_pred, average='weighted')
+
+    '''
+    cvprec = cross_val_score(estimator=model, X=features, y=labels, cv=10, scoring='precision_weighted')
+    cvrec = cross_val_score(estimator=model, X=features, y=labels, cv=10, scoring='recall_weighted')
+    cvf1 = cross_val_score(estimator=model, X=features, y=labels, cv=10, scoring='f1_weighted')
+    '''
+
 
     acc=accuracy_score( testlabels, y_pred )
 
@@ -62,14 +126,7 @@ def span_detect(model, trainfeatures, trainlabels, testfeatures, testlabels):
     #return confusion_matrix(testlabels, predicted_labels, labels=list(uniqLabels))
     #return confusion_matrix(sublabels, predicted_labels, labels=big_families)
     return (prec, rec, f1, acc)
-
-
-def selectFeatures(features, selection):
-    featureSelect=[idx-1 for idx in selection]
-    selectedfeatures=list()
-    for featureRow in features:
-        selectedfeatures.append ( featureRow[ featureSelect ] )
-    return selectedfeatures
+    #return (numpy.average(cvprec), numpy.average(cvrec), numpy.average(cvf1), acc)
 
 def malwareCatStat(labels):
     l2c={}
@@ -79,25 +136,19 @@ def malwareCatStat(labels):
         l2c[lab]=l2c[lab]+1
     return l2c
 
-def predict(bf1, bl1, bf2, bl2, fh):
-    (trainfeatures, trainlabels) = adapt (bf1, bl1)
-    (testfeatures, testlabels) = adapt (bf2, bl2)
+def predict(f, l, fh):
 
-    print "======== in training dataset ======="
-    l2c = malwareCatStat(trainlabels)
-    for lab in l2c.keys():
-        print "%s\t%s" % (lab, l2c[lab])
+    (features, labels) = adapt (f, l)
 
-    print "======== in testing dataset ======="
-    l2c = malwareCatStat(testlabels)
+    print "======== in dataset ======="
+    l2c = malwareCatStat(labels)
     for lab in l2c.keys():
         print "%s\t%s" % (lab, l2c[lab])
 
     uniqLabels = set()
-    for item in testlabels:
+    for item in labels:
         uniqLabels.add (item)
 
-    #models = (RandomForestClassifier(n_estimators = 100, random_state=0), )#ExtraTreesClassifier(n_estimators=120), GradientBoostingClassifier(n_estimators=120), BaggingClassifier (n_estimators=120), SVC(kernel='linear'), DecisionTreeClassifier(random_state=None), KNeighborsClassifier(n_neighbors=5), MultinomialNB())
     models = (SVC(), LinearSVC(C=0.01, penalty='l1', dual=False), LogisticRegression(max_iter=1000))
 
     print >> fh, '\t'.join(uniqLabels)
@@ -105,7 +156,7 @@ def predict(bf1, bl1, bf2, bl2, fh):
     model2ret={}
     for model in models:
         print >> fh, 'model ' + str(model)
-        ret = span_detect (model, trainfeatures, trainlabels, testfeatures, testlabels)
+        ret = holdout(model, features, labels)
         model2ret[str(model)] = ret
 
     tlabs=('precision', 'recall', 'F1', 'accuracy')
@@ -247,10 +298,9 @@ if __name__=="__main__":
     fh = sys.stdout
     #fh = file ('confusion_matrix_formajorfamilyonly_holdout_all.txt', 'w')
 
-    for i in range(0, len(datasets)-1):
+    for i in range(0, len(datasets)):
         g_fnames=set()
-        # training dataset
-        #(bf1, bl1) = loadMamaFeatures(datasets[i]['benign'][0], mode, "BENIGN")
+        print "same-period hold-out evaluation: work on %s ... " % ( datasets[i] )
         (bft, blt) = ({}, {})
         for k in range(0, len(datasets[i]['benign'])):
             (bf, bl) = loadFeatures(datasets[i]['benign'][k], "BENIGN")
@@ -261,32 +311,11 @@ if __name__=="__main__":
             bft.update (mf)
             blt.update (ml)
 
-        for j in range(i+1, len(datasets)):
-            print "train on %s ... test on %s ..." % ( datasets[i], datasets[j] )
+        resetframe()
 
-            # testing dataset
-            (bfp, blp) = ({}, {})
-            for k in range(0, len(datasets[j]['benign'])):
-                (bf, bl) = loadFeatures(datasets[j]['benign'][k], "BENIGN")
-                bfp.update (bf)
-                blp.update (bl)
-            for k in range(0, len(datasets[j]['malware'])):
-                (mf, ml) = loadFeatures(datasets[j]['malware'][k], "MALICIOUS")
-                bfp.update (mf)
-                blp.update (ml)
+        _bft = regularizeFeatures ( bft )
 
-            resetframe()
-
-            _bft = regularizeFeatures ( bft )
-            _bfp = regularizeFeatures ( bfp )
-
-            predict(getfvec(_bft),blt, getfvec(_bfp),blp, fh)
-
-            fhfeatures = file ('revealdroid_features_names-train-on-'+datasets[i]+'-test-on-'+datasets[j]+'.txt', 'w+')
-            for name in g_fnames:
-                print >> fhfeatures, name
-
-            fhfeatures.close()
+        predict(getfvec(_bft),blt, fh)
 
     fh.flush()
     fh.close()
