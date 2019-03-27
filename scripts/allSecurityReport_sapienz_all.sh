e4aaea6a8ed3d4bc9b14136571d3630af34c7ef0
Haipeng Cai <hcai@hcai-dl580>
2019-03-27 11:54:43 -0700

    further tuned scripts for computing more malware-2017 samples

---
+++ scripts/allSecurityReport_sapienz_all.sh
@@ -0,0 +1,5 @@
+allSecurityReport.sh /home/hcai/Downloads/VirusShare/2013/ /home/hcai/testbed/sapienzVirusShareLogs/2013/ vsresults_sapienz/2013
+allSecurityReport.sh /home/hcai/Downloads/VirusShare/2014/ /home/hcai/testbed/sapienzVirusShareLogs/2014/ vsresults_sapienz/2014
+allSecurityReport.sh /home/hcai/Downloads/VirusShare/2015/ /home/hcai/testbed/sapienzVirusShareLogs/2015/ vsresults_sapienz/2015
+allSecurityReport.sh /home/hcai/Downloads/VirusShare/2016/ /home/hcai/testbed/sapienzVirusShareLogs/2016/ vsresults_sapienz/2016
+
